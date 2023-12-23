6cf1353c7c5c8f9343e4e874f35a14248ea5ee6e
Nikolay Nikolaev <xaknick@gmail.com>
2023-12-23 20:48:15 +0300

    Fix identation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "c_sharp" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "c_sharp" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
 }
 
