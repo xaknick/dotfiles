e20f7a17690bd51f61c3e4ddb4bd320eabd1ec39
Nikolay Nikolaev <xaknick@gmail.com>
2024-07-23 14:42:50 +0200

    [nvim] Rewrite the general configuration

--- nvim/.config/nvim/lua/nvim-options.lua
+++ nvim/.config/nvim/lua/nvim-options.lua
@@ -1,6 +1,23 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+opt.termguicolors = true
+
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 vim.g.mapleader = " "
 
