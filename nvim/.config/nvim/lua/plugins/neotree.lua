ee721041350718d119bfef68dd5975d55958fe7f
Nikolay Nikolaev <xaknick@gmail.com>
2024-07-24 11:24:38 +0200

    [nvim] Configure neo-tree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,7 +8,21 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+    local keymap = vim.keymap
+
+    keymap.set('n', '<leader>ee', ':Neotree filesystem reveal left<CR>', {})
+    keymap.set('n', '<leader>eb', ':Neotree buffers<CR>', {})
+    keymap.set('n', '<leader>eg', ':Neotree float git_status<CR>', {})
+
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      filesystem = {
+        filtered_items = {
+          visible = true
+        }
+      }
+    })
   end
 }
 
