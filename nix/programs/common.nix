7e6a771f747358b49858bb2ccc220cd7bed86ebe
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-20 00:30:14 +0200

    [nix] Change ssh to https git links

--- nix/programs/common.nix
+++ nix/programs/common.nix
@@ -44,7 +44,7 @@
   # Activation script to run stow using its Nix store path
   home.activation.stow = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "${config.home.homeDirectory}/dotfiles" ]; then
-      ${pkgs.git}/bin/git clone git@github.com:xaknick/dotfiles.git ${config.home.homeDirectory}/dotfiles
+      ${pkgs.git}/bin/git clone https://github.com/xaknick/dotfiles.git ${config.home.homeDirectory}/dotfiles
     fi
     echo "Running stow for dotfiles"
     cd ${config.home.homeDirectory}/dotfiles
@@ -55,7 +55,7 @@
   home.activation.installZap = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "${config.home.homeDirectory}/.local/share/zap" ]; then
       echo "Cloning zap"
-      ${pkgs.git}/bin/git clone git@github.com:zap-zsh/zap.git ${config.home.homeDirectory}/.local/share/zap
+      ${pkgs.git}/bin/git clone https://github.com/zap-zsh/zap.git ${config.home.homeDirectory}/.local/share/zap
     else
       echo "Zap is already installed"
     fi
