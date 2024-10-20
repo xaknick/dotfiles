46767ebc15908045feb84f59d588d000e4f5bfc5
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-20 20:06:38 +0200

    [nvim] Add new neovim options

--- nvim/.config/nvim/lua/nvim-options.lua
+++ nvim/.config/nvim/lua/nvim-options.lua
@@ -1,4 +1,6 @@
 local opt = vim.opt
+opt.backup = false
+opt.swapfile = false
 
 opt.relativenumber = true
 opt.number = true
@@ -9,16 +11,22 @@ opt.shiftwidth = 2
 opt.autoindent = true
 
 opt.wrap = false
+opt.scrolloff = 4
+opt.sidescrolloff = 8
+opt.numberwidth = 2
 
 opt.ignorecase = true
 opt.smartcase = true
 
 opt.cursorline = true
+opt.showmode = false
 
 opt.termguicolors = true
 
+opt.mouse = 'a' -- Mouse mode "all"
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.whichwrap = 'bs<>[]hl' -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
 
 -- Keymaps
 vim.g.mapleader = " "
