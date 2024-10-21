391af840643c9872870ce08b2f39e1ba1a77d82f
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-21 21:06:26 +0200

    [nvim] Transparency plugin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,30 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd[[colorscheme tokyonight]]
-  end
-}
+	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+				},
+			})
+			require("transparent").clear_prefix("NeoTree")
+			require("transparent").clear_prefix("Telescope")
 
+			vim.keymap.set(
+				"n",
+				"<leader>tt",
+				":TransparentToggle<CR>",
+				{ desc = "Toggle transparency", noremap = true, silent = true }
+			)
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+}
