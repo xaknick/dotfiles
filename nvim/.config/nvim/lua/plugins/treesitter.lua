7b6757b1499230474f6976d532083287975577f1
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-21 17:46:25 +0200

    [nvim] Enhance the treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,8 +4,9 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "c_sharp" },
+      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "c_sharp", "typescript", "nix" },
       sync_install = false,
+      autoinstall = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
