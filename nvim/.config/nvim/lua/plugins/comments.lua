27198ef5ac0d0e4fdf0af5d2fab84b58cc46b93c
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-21 17:50:58 +0200

    [nvim] Comments plugin

---
+++ nvim/.config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,9 @@
+return {
+  'numToStr/Comment.nvim',
+  opts = {},
+  config = function()
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('n', '<C-/>', require('Comment.api').toggle.linewise.current, opts)
+    vim.keymap.set('v', '<C-/>', "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", opts)
+  end,
+}
