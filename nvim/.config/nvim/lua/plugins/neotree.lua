314d36b591ab61f30f4cc50fa95df550d0acca50
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-22 17:33:22 +0200

    [nvim] Enhance neotree config

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,31 +1,37 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    local keymap = vim.keymap
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		local keymap = vim.keymap
 
-    keymap.set('n', '<leader>ee', ':Neotree filesystem reveal left<CR>', {})
-    keymap.set('n', '<leader>eb', ':Neotree buffers<CR>', {})
-    keymap.set('n', '<leader>eg', ':Neotree float git_status<CR>', {})
+		keymap.set("n", "<leader>ee", ":Neotree filesystem reveal left<CR>", {})
+		keymap.set("n", "<leader>eb", ":Neotree buffers<CR>", {})
+		keymap.set("n", "<leader>eg", ":Neotree float git_status<CR>", {})
 
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      popup_border_style = "rounded",
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_by_name = {
-            ".git"
-          }
-        }
-      }
-    })
-  end
+		require("neo-tree").setup({
+			sources = {
+				"filesystem",
+				"buffers",
+				"git_status",
+				"document_symbols",
+			},
+			close_if_last_window = true,
+			popup_border_style = "rounded",
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_by_name = {
+						".git",
+					},
+				},
+				use_libuv_file_watcher = true,
+			},
+		})
+	end,
 }
-
