1f3fcfef16ca42da951626ffbb2f1dce95cadf64
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-22 19:29:47 +0200

    [nvim] Add new hotkeys

--- nvim/.config/nvim/lua/nvim-keymap.lua
+++ nvim/.config/nvim/lua/nvim-keymap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
 -- Exit insert mode with jk
@@ -30,3 +31,13 @@ vim.keymap.set('v', '>', '>gv', opts)
 vim.keymap.set('v', 'p', '"_dP', opts)
 
 vim.keymap.set('n', '<leader>ch', '<cmd>checkhealth<CR>', opts)
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
