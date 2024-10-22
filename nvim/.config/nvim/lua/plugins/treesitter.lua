d9e9bd07a286dfee9bb817fee1e66d503dcff15a
Nikolay Nikolaev <xaknick@gmail.com>
2024-10-22 11:25:51 +0200

    [nvim] Incremental selection

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,7 +9,15 @@ return {
       autoinstall = true,
       highlight = { enable = true },
       indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
+          node_incremental = "<leader>si",
+          scope_incremental = "<leader>sc",
+          node_decremental = "<leader>sd",
+        },
+      },
     })
-  end
+  end,
 }
-
