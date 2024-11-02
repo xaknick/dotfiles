31053aaf483440fcbcc5867f09ef06d9f397a776
Nikolay Nikolaev <xaknick@gmail.com>
2024-11-02 23:13:28 +0500

    [wezterm] Change cursor to the bar

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,6 +10,8 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+config.default_cursor_style = 'SteadyBar'
+
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font_with_fallback {
   'MesloLGS NF',
