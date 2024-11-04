6de7de79a34f46d488c9dd907c83f995901e556b
Nikolay Nikolaev <xaknick@gmail.com>
2024-11-05 00:29:53 +0500

    [nvim] Yamlls, absiblels in mason lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -13,26 +13,30 @@ return {
         },
       })
 
-      lspconfig.nil_ls.setup({
-        settings = {
-          ["nil"] = {
-            formatting = {
-              command = { "nixfmt" }, -- optional, use your formatter if needed
-            },
-          },
-        },
-      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
       local os_utils = require("os_utils")
 
-       -- Skip installation Mason for NixOS 
+      -- Install nix lsp for NixOS only
+      if os_utils.is_nixos() then
+        lspconfig.nil_ls.setup({
+          settings = {
+            ["nil"] = {
+              formatting = {
+                command = { "nixfmt" }, -- optional, use your formatter if needed
+              },
+            },
+          },
+        })
+      end
+
+      -- Skip installation Mason for NixOS
       if not os_utils.is_nixos() then
         require("mason").setup()
         require("mason-lspconfig").setup({
-          ensure_installed = { "lua_ls" }, -- Add any additional LSPs here
+          ensure_installed = { "lua_ls", "ansiblels", "yamlls" }, -- Add any additional LSPs here
         })
       end
     end,
