5192d83f2f4143e60975a190b5c97ba4993c7c30
Nikolay Nikolaev <xaknick@gmail.com>
2024-12-13 11:41:43 +0100

    [nvim] Install treesitter text objects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "c_sharp", "typescript", "nix" },
-      sync_install = false,
-      autoinstall = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
-          node_incremental = "<leader>si",
-          scope_incremental = "<leader>sc",
-          node_decremental = "<leader>sd",
-        },
-      },
-    })
-  end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "c_sharp", "typescript", "nix" },
+				sync_install = false,
+				autoinstall = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
+						node_incremental = "<leader>si",
+						scope_incremental = "<leader>sc",
+						node_decremental = "<leader>sd",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						},
+						selection_modes = {
+							["@parameter.outer"] = "v", -- charwise
+							["@function.outer"] = "V", -- linewise
+							["@class.outer"] = "<c-v>", -- blockwise
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 }
