096b678f897cfc83e3963ea5208fa64ad9452fb9
Nikolay Nikolaev <xaknick@gmail.com>
2025-04-29 22:53:17 +0200

    [nvim] Refactor lsp config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -2,8 +2,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local os_utils = require("os_utils")
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "yamlls",
+          "gopls",
+        },
+      })
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua",
+          "yamllint",
+          "yamlfmt",
+        },
+      })
+
       local lspconfig = require("lspconfig")
+      -- Lua
       lspconfig.lua_ls.setup({
         settings = {
           Lua = {
@@ -13,9 +29,7 @@ return {
           },
         },
       })
-
-      lspconfig.jsonls.setup({})
-      lspconfig.cssls.setup({})
+      -- Golang
       lspconfig.gopls.setup({
         settings = {
           gopls = {
@@ -23,41 +37,40 @@ return {
           },
         },
       })
-
-      if os_utils.is_regular_unix() then
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "lua_ls",
-            "yamlls",
-            "gopls",
-          },
-        })
-        require("mason-tool-installer").setup({
-          ensure_installed = {
-            "stylua",
-            "yamllint",
-            "yamlfmt",
-          },
-        })
-      end
+      -- Others
+      lspconfig.jsonls.setup({})
+      lspconfig.cssls.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
       -- Install nix lsp for NixOS only
-      if os_utils.is_nixos() then
-        lspconfig.nil_ls.setup({
-          settings = {
-            ["nil"] = {
-              formatting = {
-                command = { "nixfmt" }, -- optional, use your formatter if needed
-              },
-            },
-          },
-        })
-      end
+      -- Ucomment lines below on NixOS
+      -- local os_utils = require("os_utils")
+      -- if os_utils.is_nixos() then
+      --   lspconfig.nil_ls.setup({
+      --     settings = {
+      --       ["nil"] = {
+      --         formatting = {
+      --           command = { "nixfmt" }, -- optional, use your formatter if needed
+      --         },
+      --       },
+      --     },
+      --   })
+      -- elseif os_utils.is_regular_unix() then
+      --   require("mason").setup()
+      --   require("mason-lspconfig").setup({
+      --     ensure_installed = {
+      --       "lua_ls",
+      --     },
+      --   })
+      --   require("mason-tool-installer").setup({
+      --     ensure_installed = {
+      --       "stylua",
+      --     },
+      --   })
+      -- end
     end,
   },
   {
