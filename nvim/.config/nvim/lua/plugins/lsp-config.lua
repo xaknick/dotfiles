0a684a7276d40ae5d8b249992408def67b392f51
Nikolay Nikolaev <xaknick@gmail.com>
2025-05-02 22:37:25 +0200

    [nvim] Remove nixos lsp code

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -44,33 +44,6 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-
-      -- Install nix lsp for NixOS only
-      -- Ucomment lines below on NixOS
-      -- local os_utils = require("os_utils")
-      -- if os_utils.is_nixos() then
-      --   lspconfig.nil_ls.setup({
-      --     settings = {
-      --       ["nil"] = {
-      --         formatting = {
-      --           command = { "nixfmt" }, -- optional, use your formatter if needed
-      --         },
-      --       },
-      --     },
-      --   })
-      -- elseif os_utils.is_regular_unix() then
-      --   require("mason").setup()
-      --   require("mason-lspconfig").setup({
-      --     ensure_installed = {
-      --       "lua_ls",
-      --     },
-      --   })
-      --   require("mason-tool-installer").setup({
-      --     ensure_installed = {
-      --       "stylua",
-      --     },
-      --   })
-      -- end
     end,
   },
   {
