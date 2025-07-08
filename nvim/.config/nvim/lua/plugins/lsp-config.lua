7709302bfd8da3cb2023375007f51f8598466dc0
Nikolay Nikolaev <xaknick@gmail.com>
2025-07-08 20:23:03 +0300

    [nvim] Add ts_ls to lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -37,6 +37,27 @@ return {
           },
         },
       })
+      -- TypeScript
+      lspconfig.ts_ls.setup({
+        -- Optional: customize filetypes, root_dir, or settings
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
+        root_dir = lspconfig.util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git"),
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+            tsserver = {
+              useSyntaxServer = false,
+            },
+          },
+        },
+      })
       -- Others
       lspconfig.jsonls.setup({})
       lspconfig.cssls.setup({})
