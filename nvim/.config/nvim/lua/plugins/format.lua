59bb99d1f3a5020fbd1298dc1625d4f3438850d1
Nikolay Nikolaev <xaknick@gmail.com>
2025-08-15 16:53:34 +0200

    [nvim] Conform plugin for formatting

---
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -0,0 +1,15 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			yaml = { "yamlfmt" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
