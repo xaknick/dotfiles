484d756623215eeb8b7472be2339e631c6b9ef77
Nikolay Nikolaev <xaknick@gmail.com>
2025-08-23 15:41:58 +0200

    [nvim] Add DAP plugins

---
+++ nvim/.config/nvim/lua/plugins/dap-hotkeys.lua
@@ -0,0 +1,66 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		keys = function()
+			local dap = require("dap")
+			return {
+				{ "<F5>", dap.continue, desc = "DAP Continue", mode = "n" },
+				{ "<F10>", dap.step_over, desc = "DAP Step Over", mode = "n" },
+				{ "<F11>", dap.step_into, desc = "DAP Step Into", mode = "n" },
+				{ "<F12>", dap.step_out, desc = "DAP Step Out", mode = "n" },
+				{ "<F9>", dap.toggle_breakpoint, desc = "DAP Toggle BP", mode = "n" },
+				{
+					"<leader>B",
+					function()
+						vim.ui.input({ prompt = "Breakpoint condition: " }, function(input)
+							if input and input ~= "" then
+								dap.set_breakpoint(input)
+							else
+								dap.set_breakpoint()
+							end
+						end)
+					end,
+					desc = "DAP Conditional BP",
+					mode = "n",
+				},
+				{
+					"<leader>dr",
+					function()
+						require("dap").repl.toggle()
+					end,
+					desc = "DAP REPL",
+					mode = "n",
+				},
+				{
+					"<leader>dl",
+					function()
+						require("dap").run_last()
+					end,
+					desc = "DAP Run Last",
+					mode = "n",
+				},
+				{
+					"<leader>dq",
+					function()
+						require("dap").terminate()
+					end,
+					desc = "DAP Terminate",
+					mode = "n",
+				},
+			}
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		keys = {
+			{
+				"<leader>du",
+				function()
+					require("dapui").toggle()
+				end,
+				desc = "DAP UI Toggle",
+				mode = "n",
+			},
+		},
+	},
+}
---
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,59 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError", linehl = "", numhl = "" })
+			vim.fn.sign_define(
+				"DapBreakpointCondition",
+				{ text = "◆", texthl = "DiagnosticWarn", linehl = "", numhl = "" }
+			)
+			vim.fn.sign_define("DapStopped", { text = "▶", texthl = "DiagnosticInfo", linehl = "Visual", numhl = "" })
+
+			-- Load langs configurations
+			require("plugins.dap-lang")
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		dependencies = { "mfussenegger/nvim-dap" },
+		opts = { virt_text_pos = "eol", all_frames = false },
+	},
+	{
+		"nvim-telescope/telescope-dap.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap" },
+		config = function()
+			require("telescope").load_extension("dap")
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+		opts = {
+			automatic_setup = true,
+			ensure_installed = {
+				"coreclr", -- .NET (netcoredbg)
+				"delve", -- Go (dlv)
+			},
+		},
+		config = function(_, opts)
+			require("mason-nvim-dap").setup(opts)
+		end,
+	},
+}
