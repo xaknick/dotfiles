d10b6ed40262b14a35dbce711f4d3a8431c04dc1
Nikolay Nikolaev <xaknick@gmail.com>
2025-08-28 01:06:46 +0200

    [zsh] Make nvim config alias as a function

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,4 +3,9 @@ alias v="nvim"
 alias ls="eza -a -1 --group-directories-first"
 alias mc="mc -u"
 alias lg="lazygit"
-alias vc="nvim ~/.config/nvim/"
+
+vc() {
+  local cfg="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+  builtin cd -- "$cfg" || return
+  nvim .
+}
