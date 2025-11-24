e3d4f957e08f244c3e776ffa016ec37662a0bef2
Nikolay Nikolaev <xaknick@gmail.com>
2025-11-24 12:37:02 +0100

    [zsh] Add a function to ask aichat using the current line as a promt

---
+++ zsh/.config/zsh/plugins/ai.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+_aichat_zsh() {
+    if [[ -n "$BUFFER" ]]; then
+        local _old=$BUFFER
+        BUFFER+="⌛"
+        zle -I && zle redisplay
+        BUFFER=$(aichat -e "$_old")
+        zle end-of-line
+    fi
+}
+zle -N _aichat_zsh
+# Alt + E to send the current line as a promt
+bindkey '\ee' _aichat_zsh
