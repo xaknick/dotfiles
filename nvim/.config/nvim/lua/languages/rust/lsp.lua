fd80f31c81b7161cb96cc43b542c21da50782ee3
Nikolay Nikolaev <xaknick@gmail.com>
2026-03-29 01:57:06 +0100

    [nvim] Rust LSP configuration

--- nvim/.config/nvim/lua/languages/rust/lsp.lua
+++ nvim/.config/nvim/lua/languages/rust/lsp.lua
@@ -3,7 +3,38 @@ local M = {}
 M.servers = { "rust_analyzer" }
 
 function M.setup()
-	-- Using default rust_analyzer configuration
+	vim.lsp.config("rust_analyzer", {
+		settings = {
+			["rust-analyzer"] = {
+				cargo = {
+					allFeatures = true,
+					buildScripts = {
+						enable = true,
+					},
+				},
+				procMacro = {
+					enable = true,
+				},
+				check = {
+					command = "clippy",
+				},
+				imports = {
+					granularity = {
+						group = "module",
+					},
+					prefix = "self",
+				},
+				completion = {
+					autoimport = {
+						enable = true,
+					},
+				},
+				diagnostics = {
+					enable = true,
+				},
+			},
+		},
+	})
 end
 
 return M
