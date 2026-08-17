1c6b64f0c2fd84b7b2dff9cb49474d30e5ff762b
Nikolay Nikolaev <xaknick@gmail.com>
2026-08-17 14:58:20 +0200

    [nvim] Migrate treesitter to main API

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,97 @@
+local parsers = {
+	"lua",
+	"vim",
+	"vimdoc",
+	"query",
+	"yaml",
+	"toml",
+	"json",
+	"javascript",
+	"typescript",
+	"python",
+	"go",
+	"rust",
+	"c_sharp",
+}
+
+local filetypes = {
+	"lua",
+	"vim",
+	"help",
+	"query",
+	"yaml",
+	"toml",
+	"json",
+	"javascript",
+	"typescript",
+	"python",
+	"go",
+	"rust",
+	"cs",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = {
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"yaml",
-					"toml",
-					"json",
-					"typescript",
-					"rust",
-					"c_sharp",
-				},
-				sync_install = false,
-				auto_install = true,
-				highlight = { enable = true },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
-						node_incremental = "<leader>si",
-						scope_incremental = "<leader>sc",
-						node_decremental = "<leader>sd",
-					},
-				},
-				modules = {},
-				ignore_install = {},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-						},
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-					},
-				},
+			local treesitter = require("nvim-treesitter")
+			treesitter.setup()
+			treesitter.install(parsers)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = filetypes,
+				callback = function()
+					vim.treesitter.start()
+				end,
 			})
+
+			vim.keymap.set("n", "<leader>ss", function()
+				vim.treesitter.select("parent")
+			end, { desc = "Start treesitter selection" })
+			vim.keymap.set("x", "<leader>si", function()
+				vim.treesitter.select("parent")
+			end, { desc = "Expand treesitter selection" })
+			vim.keymap.set("x", "<leader>sc", function()
+				vim.treesitter.select("parent")
+			end, { desc = "Expand treesitter scope" })
+			vim.keymap.set("x", "<leader>sd", function()
+				vim.treesitter.select("child")
+			end, { desc = "Shrink treesitter selection" })
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		lazy = false,
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+					selection_modes = {
+						["@parameter.outer"] = "v",
+						["@function.outer"] = "V",
+						["@class.outer"] = "<c-v>",
+					},
+				},
+			})
+
+			local select = require("nvim-treesitter-textobjects.select").select_textobject
+			local keymaps = {
+				["af"] = { "@function.outer", "Select outer function" },
+				["if"] = { "@function.inner", "Select inner function" },
+				["ac"] = { "@class.outer", "Select outer class" },
+				["ic"] = { "@class.inner", "Select inner class" },
+				["as"] = { "@local.scope", "Select local scope", "locals" },
+			}
+
+			for key, mapping in pairs(keymaps) do
+				vim.keymap.set({ "x", "o" }, key, function()
+					select(mapping[1], mapping[3])
+				end, { desc = mapping[2] })
+			end
+		end,
 	},
 }
